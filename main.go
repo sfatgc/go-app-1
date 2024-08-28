bdd1926653794fbeacb74ab1db0c81e814ca33a4
Stepan G. Fedorov <39520445+sfatgc@users.noreply.github.com>
2024-08-28 04:33:34 -0600

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s\n", r.URL.Query().Get("name"))
 }
+
+func heavy_request(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Handling heavy workload...\n")
+	time.sleep(15)
+	fmt.Fprintf(w, "...successfully handled heavy workload\n")
+}
+
 func main() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/load", heavy_request)
 	http.ListenAndServe(":8000", nil)
 }
